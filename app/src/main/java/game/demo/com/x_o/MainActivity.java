ac557533c5cf4941d4d7f83c9187922455a3c66c
yra1908 <yra1908@gmail.com>
2016-01-04 00:14:38 +0200

    enough for today

--- app/src/main/java/game/demo/com/x_o/MainActivity.java
+++ app/src/main/java/game/demo/com/x_o/MainActivity.java
@@ -1,5 +1,6 @@
 package game.demo.com.x_o;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -24,10 +26,16 @@ public class MainActivity extends AppCompatActivity
     implements OnClickListener {
 
     public static final String LOG_TAG="Log_Debug";
+
     private static final int CELL_AMOUNT=9;
+    private static int [][] lines = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6},
+                                     {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
 
     private boolean player1;
     private List<Cell> field;
+    private int count;
+
+    TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +44,13 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        status = (TextView) findViewById(R.id.status);
+
         //setting onclick listener to layout
         setListeners();
 
         //Setting cell params
         setCellGrid();
-
-    }
-
-    private void setCellGrid(){
-        player1=true;
-        field = new ArrayList<>();
-        for (int i=0; i<CELL_AMOUNT; i++) {
-            field.add(new Cell(i));
-        }
 
     }
 
@@ -81,21 +82,15 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_exit) {
+            System.exit(0);
         }
 
         return super.onOptionsItemSelected(item);
@@ -115,86 +110,152 @@ public class MainActivity extends AppCompatActivity
                 if(!checkField(0)){
                     return;
                 }
-                field.get(0).setActive(true);
+                prepareCell(0);
                 imView =(ImageView) findViewById(R.id.imgCel1);
                 break;
             case R.id.cel2:
                 if(!checkField(1)){
                     return;
                 }
-                field.get(1).setActive(true);
+                prepareCell(1);
                 imView =(ImageView) findViewById(R.id.imgCel2);
                 break;
             case R.id.cel3:
                 if(!checkField(2)){
                     return;
                 }
-                field.get(2).setActive(true);
+                prepareCell(2);
                 imView =(ImageView) findViewById(R.id.imgCel3);
                 break;
             case R.id.cel4:
                 if(!checkField(3)){
                     return;
                 }
-                field.get(3).setActive(true);
+                prepareCell(3);
                 imView =(ImageView) findViewById(R.id.imgCel4);
                 break;
             case R.id.cel5:
                 if(!checkField(4)){
                     return;
                 }
-                field.get(4).setActive(true);
+                prepareCell(4);
                 imView =(ImageView) findViewById(R.id.imgCel5);
                 break;
             case R.id.cel6:
                 if(!checkField(5)){
                     return;
                 }
-                field.get(5).setActive(true);
+                prepareCell(5);
                 imView =(ImageView) findViewById(R.id.imgCel6);
                 break;
             case R.id.cel7:
                 if(!checkField(6)){
                     return;
                 }
-                field.get(6).setActive(true);
+                prepareCell(6);
                 imView =(ImageView) findViewById(R.id.imgCel7);
                 break;
             case R.id.cel8:
                 if(!checkField(7)){
                     return;
                 }
-                field.get(7).setActive(true);
+                prepareCell(7);
                 imView =(ImageView) findViewById(R.id.imgCel8);
                 break;
             case R.id.cel9:
                 if(!checkField(8)){
                     return;
                 }
-                field.get(8).setActive(true);
+                prepareCell(8);
                 imView =(ImageView) findViewById(R.id.imgCel9);
                 break;
         }
 
-        String imageName;mm""
-        if(player1){
+        String imageName;
+        if (player1) {
             imageName = "cross";
             player1=false;
+            status.setText("Player 2 Move");
         } else {
             imageName = "zero";
             player1=true;
+            status.setText("Player 1 Move");
         }
 
         int res = getResources().getIdentifier(imageName, "drawable", getPackageName());
 
         imView.setImageResource(res);
+        if(count>4){
+            if(checkLines()){
+                finishGame();
+            }
+        }
     }
+
+    private void setCellGrid(){
+        count=0;
+        player1=true;
+        status.setText("Player1 Move");
+        field = new ArrayList<>();
+        for (int i=0; i<CELL_AMOUNT; i++) {
+            field.add(new Cell(i));
+        }
+
+    }
+
+    private void prepareCell(int number){
+        field.get(number).setActive(true);
+        if(player1){
+            field.get(number).setValue(Cell.Value.CROSS);
+        } else {
+            field.get(number).setValue(Cell.Value.ZERO);
+        }
+    }
+
+    private boolean checkLines(){
+
+        int lineNumber = 1;
+        for (int[] line: lines) {
+
+            if (field.get(line[0]).equals(field.get(line[1])) &&
+                    field.get(line[0]).equals(field.get(line[2])) &&
+                    !field.get(line[0]).getValue().equals(Cell.Value.NONE)){
+
+                setLineBackground(lineNumber);
+                return true;
+            }
+            lineNumber++;
+        }
+        return false;
+    }
+
+    private void setLineBackground(int i) {
+        ImageView imView1 =(ImageView) findViewById(R.id.imgCel1);
+        imView1.setBackgroundColor(Color.parseColor("#3F51B5"));
+    }
+
+    private void finishGame(){
+        if(player1){
+            status.setText("Player2 Win!");
+        } else {
+            status.setText("Player1 Win!");
+        }
+        for (Cell cell: field) {
+            cell.setActive(true);
+        }
+
+    }
+
+
+
+
 
     private boolean checkField(int number){
         if(field.get(number).isActive()){
             Toast.makeText(this, "Field is already filled", Toast.LENGTH_SHORT).show();
             return false;
         }
+        count++;
         return true;
     }
 
@@ -218,5 +279,7 @@ public class MainActivity extends AppCompatActivity
         imView7.setImageDrawable(null);
         imView8.setImageDrawable(null);
         imView9.setImageDrawable(null);
+
+        imView1.setBackgroundColor(Color.TRANSPARENT);
     }
 }
--- app/src/main/java/game/demo/com/x_o/model/Cell.java
+++ app/src/main/java/game/demo/com/x_o/model/Cell.java
@@ -5,12 +5,13 @@ package game.demo.com.x_o.model;
  */
 public class Cell {
 
-    //int number;
     private boolean active;
+    private Value value;
+
 
     public Cell(int number){
-        //this.number=number;
         active=false;
+        this.value=Value.NONE;
     }
 
     public boolean isActive() {
@@ -19,5 +20,33 @@ public class Cell {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    public enum Value{
+        CROSS, ZERO, NONE
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cell cell = (Cell) o;
+
+        return value == cell.value;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return value != null ? value.hashCode() : 0;
     }
 }
